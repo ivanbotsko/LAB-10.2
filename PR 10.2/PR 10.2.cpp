8419caa7055d64332f3238603e1446063a12f862
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-12-07 11:43:58 +0200

    first master commit

---
+++ PR 10.2/PR 10.2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string deleteBetweenBrackets(const string& str) {
+    string result = "";
+    int openBracketsCount = 0;
+
+    for (char ch : str) {
+        if (ch == '(') {
+            openBracketsCount++;
+        }
+        else if (ch == ')' && openBracketsCount > 0) {
+            openBracketsCount--;
+        }
+        else if (openBracketsCount == 0) {
+            result += ch;
+        }
+    }
+
+    return result;
+}
+
+void processFile(const string& inputFile, const string& outputFile) {
+    ifstream inFile(inputFile);
+    ofstream outFile(outputFile);
+
+    if (inFile.is_open() && outFile.is_open()) {
+        string line;
+        while (getline(inFile, line)) {
+            string processedLine = deleteBetweenBrackets(line);
+            outFile << processedLine << endl;
+        }
+        inFile.close();
+        outFile.close();
+        cout << "File processing complete." << endl;
+    }
+    else {
+        cout << "Unable to open files." << endl;
+    }
+}
+
+int main() {
+    string inputFileName = "t1.txt"; // Змінити на відповідні імена файлів
+    string outputFileName = "t2.txt"; // Змінити на відповідні імена файлів
+
+    processFile(inputFileName, outputFileName);
+
+    return 0;
+}
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR 10.2/PR 10.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace YourProjectTests
+{
+    TEST_CLASS(YourFileTests)
+    {
+    public:
+
+        TEST_METHOD(TestDeleteBetweenBrackets)
+        {
+            // Вхідні дані
+            std::string testString1 = "This is a (test) string.";
+            std::string testString2 = "No brackets here.";
+            std::string testString3 = "(Nested (brackets) are (here)).";
+
+            // Очікуваний результат
+            std::string expected1 = "This is a  string.";
+            std::string expected2 = "No brackets here.";
+            std::string expected3 = ".";
+
+            // Виклик функції deleteBetweenBrackets
+            std::string result1 = deleteBetweenBrackets(testString1);
+            std::string result2 = deleteBetweenBrackets(testString2);
+            std::string result3 = deleteBetweenBrackets(testString3);
+
+            // Перевірка результатів
+            Assert::IsTrue(expected1 == result1, L"Test 1 failed");
+            Assert::IsTrue(expected2 == result2, L"Test 2 failed");
+            Assert::IsTrue(expected3 == result3, L"Test 3 failed");
+        }
+    };
+}
+
+
